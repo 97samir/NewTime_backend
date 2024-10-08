39fc5ac2a30c5571bf40b89577b70a687c8fc432
97samir <samirchura97@gmail.com>
2024-10-07 19:59:06 -0500

    borrando lineas innecesarias

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Conexión exitosa a MongoDB Atlas"))
     .catch((error) => console.error("Error conectando a MongoDB Atlas: ", error));
 
-
-app.use(cors({
-    origin: 'https://newtimee.vercel.app', // Cambia a la URL de tu frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+//autoriza al navegador para que pueda enviar solicitud, transferencia de datos
+// app.use(cors({
+//     origin: 'https://newtimee.vercel.app', // Cambia a la URL de tu frontend
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
 
 
 // Rutas
@@ -29,12 +30,6 @@ app.use('/auth', authRoutes);
 
 // Servir archivos estáticos (HTML, CSS, etc.)
 app.use(express.static(path.join(__dirname, '../frontend')));
-
-// Para cualquier otra ruta, enviar el archivo index.html del frontend
-//ruta agregada solo para subir a heroku
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-});
 
 // Iniciar servidor
 app.listen(PORT, () => {
